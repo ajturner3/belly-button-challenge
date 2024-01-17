2a8596e9b0b102d020362b4bab310de16884bc5e
Alex Turner <141861525+ajturner3@users.noreply.github.com>
2024-01-16 18:45:26 -0600

    Update app.js

--- static:js/js/app.js
+++ static:js/js/app.js
@@ -1,8 +1,6 @@
 // READ IN DATA
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-d3.json(url).then(function(data) {
-    console.log(data);
-})
+const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
+d3.json(url).then(data=>console.log(data));
 
 
 // INITALIZE FUNCTION
@@ -79,8 +77,9 @@ function metadataPanel(sample) {
     })
 };
 
+
 //DROPDOWN MENU
-function optionChanged(ID) {
+function optionChanged(sample) {
     console.log(sample);
     barChart(sample);
     bubbleChart(sample);
@@ -88,4 +87,4 @@ function optionChanged(ID) {
     gaugeChart(sample);
 };
 
-init();              
+init();    
